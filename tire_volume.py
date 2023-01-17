1beae760f49ffdb5b05d07c6066d4ab4088251d8
autive42 <eheiner1764@gmail.com>
2023-01-16 20:56:45 -0700

    added file output with date

--- tire_volume.py
+++ tire_volume.py
@@ -1,10 +1,17 @@
 import math
+from datetime import datetime
 
-width = int(input('Enter the width of the tire in mm (ex 205): '))
-ratio = int(input('Enter the aspect ratio of the tire (ex 60): '))
-diameter = int(input('Enter the diameter of the wheel in inches (ex 15): '))
+with open("volumes.txt", "at") as volumes_file:
 
-volume = (math.pi * width ** 2 * ratio * (width * ratio + 2540 * diameter))/10_000_000_000
+    width = int(input('Enter the width of the tire in mm (ex 205): '))
+    ratio = int(input('Enter the aspect ratio of the tire (ex 60): '))
+    diameter = int(input('Enter the diameter of the wheel in inches (ex 15): '))
 
-print()
-print(f'The approximate volume of the tire is {volume:.2f} liters.')
+    volume = (math.pi * width ** 2 * ratio * (width * ratio + 2540 * diameter))/10_000_000_000
+
+    now = datetime.now()
+
+    print()
+    print(f'The approximate volume of the tire is {volume:.2f} liters.')
+
+    print(f'{now:%Y-%m-%d}, {ratio}, {diameter}, {volume}', file = volumes_file)
